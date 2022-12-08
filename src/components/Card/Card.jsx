833e40b7aed6cf831171ddf4b3b8bb79de1f2c05
Andrei Nascimento <andrei.lima.nas@outlook.com>
2022-12-08 00:01:59 -0300

    first commit

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,12 @@
+import './Card.css'
+
+function Card(props) {
+    return (
+        <div className='card'>
+            <strong>{props.name}</strong>
+            <small>{props.time}</small>
+        </div>
+    )
+}
+
+export default Card;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from 'react';
+import Card from '../../components/Card/Card';
+import './Home.css';
+
+function Home() {
+    const [studentName, setStudentName] = useState('');
+    const [students, setStudents] = useState([]);
+    const [user, setUser] = useState({ name: '', avatar: ''});
+
+    function handleAddStudent() {
+
+        const newStudent = {
+            name: studentName,
+            time: new Date().toLocaleTimeString("pt-br", {
+                hour: '2-digit',
+                minute: '2-digit',
+                second: '2-digit'
+            })
+        };
+        
+        setStudents(prevState => [...prevState, newStudent]);
+    };
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch('https://api.github.com/users/andrei-nascimento')
+            const data = await response.json();
+            
+            setUser({
+                name: data.name,
+                avatar: data.avatar_url,
+            });
+        }
+        
+        fetchData();
+    }, []);
+
+    return (
+        <div className='container'>
+            
+            <header>
+                <h1>Lista de Presença</h1>
+                <div>
+                    <strong>{user.name}</strong>
+                    <img src={user.avatar} alt="Foto perfil" />
+                </div>
+            </header>
+
+            <input 
+                type='text' 
+                placeholder='Digite o nome...'
+                onChange={ e => setStudentName(e.target.value) }
+            />
+
+            <button type='button' onClick={handleAddStudent}>
+                Adicionar
+            </button>
+
+            {
+                students.map(student => (
+                    <Card 
+                        key={student.time}
+                        name={student.name}
+                        time={student.time}
+                    />
+                ))
+            }
+            
+
+        </div>
+    )
+}
+
+export default Home;
